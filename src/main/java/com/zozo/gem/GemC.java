f5184fd24843371bba5f6f4675654c4337e64d4a
Joeliowo <cillian16@pcd07.ie>
2020-04-20 18:58:21 +0100

    Epic

---
+++ src/main/java/com/zozo/gem/GemC.java
@@ -0,0 +1,9 @@
+package com.zozo.gem;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = GemC.MODID, acceptableRemoteVersions = GemC.VERSION, acceptedMinecraftVersions = GemC.VERSION)
+public class GemC {
+    public static final String MODID = "gem";
+    public static final String VERSION = "1.12.2";
+}
