bfcaff753cc9f79197536739ad6c0e4f924b90d0
Joeliowo <cillian16@pcd07.ie>
2020-04-21 20:22:17 +0100

    Set up packages

--- src/main/java/com/zozo/gem/GemC.java
+++ src/main/java/com/zozo/gem/GemC.java
@@ -1,9 +1,26 @@
 package com.zozo.gem;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = GemC.MODID, acceptableRemoteVersions = GemC.VERSION, acceptedMinecraftVersions = GemC.VERSION)
 public class GemC {
     public static final String MODID = "gem";
     public static final String VERSION = "1.12.2";
+
+    @Mod.Instance
+    public static GemC instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent e) {
+        //Pre-Initialization Event is for Loot Tables, Recipes,
+        //and setting a World Generator
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e) {
+        //Initialization is for (Tile-)Entities, Events, and World Generation
+    }
 }
